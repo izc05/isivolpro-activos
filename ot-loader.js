beab5fae6548d50298d4cd1531c680545f77f80f
izc05 <isiciozafracantos@gmail.com>
2026-06-11 05:50:48 +0200

    Force latest OT module load

--- ot-loader.js
+++ ot-loader.js
@@ -1,1 +1,13 @@
-import './ot-module.js';
+const VERSION = '20260611-1';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
+}
+
+caches?.keys?.().then((keys) => {
+  keys.forEach((key) => caches.delete(key));
+}).catch(() => {});
+
+import(`./ot-module.js?v=${VERSION}`);
