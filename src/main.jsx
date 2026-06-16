d862c32e6cee6c0f854a2908ffe374645381e98d
izc05 <isiciozafracantos@gmail.com>
2026-06-16 18:43:34 +0200

    Load active installation styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { TenantProvider } from './hooks/useTenant';
 import './styles/global.css';
 import './styles/workflow.css';
 import './styles/finalReview.css';
+import './styles/activeInstallation.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
